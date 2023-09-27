aedede9b5af94590f97fa5d8d77db4ca693fa30c
Vishal Kumar Singh <kvishal07860@gmail.com>
2023-09-27 15:48:17 +0530

    changed meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ const raleway = Raleway({ subsets: ["latin"] });
 export const metadata = {
   title: "Best Software Developer Portfolio | Vishalvoid | Vishal KR Singh",
   description:
-    "Explore Vishal KR Singh's best software developer portfolio - showcasing expertise in web & app development. Discover innovative projects, skills, and achievements.",
+    "Explore Vishal's best software developer portfolio - showcasing expertise in web & app development.",
 };
 
 export default function RootLayout({ children }) {
